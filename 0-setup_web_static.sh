6e639ed72b6cfb342850acdcb6a8ab398fe73456
nimba005 <nimbaoyugi@gmail.com>
2023-12-07 09:54:41 +0000

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,7 +15,7 @@ current_date=$(date +"%Y-%m-%d %H:%M:%S")
 html_content="<html>
   <head></head>
   <body>$body_content</body>
-  <p>Generated on: $current_data</p>
+  <p>Generated on: $current_date</p>
 </html>"
 
 echo "$html_content" | sudo tee /data/web_static/releases/test/index.html > /dev/null
