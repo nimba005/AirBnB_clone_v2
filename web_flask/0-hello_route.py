bbd92f10c22e72f3d887137231ba2a346975c987
nimba005 <nimbaoyugi@gmail.com>
2024-01-24 14:16:43 +0000

    route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,13 +9,12 @@ app = Flask(__name__)
 
 
 # Routing to the path '/'
-@app.route('/airbnb-onepage', strict_slashes=False)
-def hello_hbnb():
+@app.route("/airbnb-onepage/", methods=["GET"])
+def hello_world():
     """Return Hello HBNB"""
     return "Hello HBNB!"
 
 
 # Make it listen to other machines
 if __name__ == '__main__':
-    from gunicorn import Gunicorn
-    Gunicorn(app).run(host='0.0.0.0', port=8888)
+    app.run(host='0.0.0.0', port=5000)
